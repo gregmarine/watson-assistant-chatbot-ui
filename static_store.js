4aa10e9799825cd3257b0ffb978b0c2db5c57f91
Greg Marine <gregmarine@iccnet.org>
2020-06-23 12:50:14 -0400

    Updating local and dev urls

--- static_store.js
+++ static_store.js
@@ -1,5 +1,5 @@
-const DEV_HOST_URL = 'https://lem-assistant-chatbot-ui.mybluemix.net';
-const LOCAL_HOST_URL = 'http://localhost:3000';
+const DEV_HOST_URL = '';
+const LOCAL_HOST_URL = 'http://localhost:4000';
 
 module.exports = {
     DEV_HOST_URL,
